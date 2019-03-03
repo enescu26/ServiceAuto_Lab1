377d5eec255cb8170d8bbefb58677039fa3b5d06
aenescu <alina.enescu26@yahoo.com>
2019-03-03 21:46:10 +0200

    Project solved with Group 2

---
+++ src/Service/CarService.java
@@ -0,0 +1,116 @@
+package Service;
+
+import Domain.Car;
+import Repository.CarRepository;
+
+import java.util.*;
+
+public class CarService {
+
+    private CarRepository repository;
+
+    /**
+     * ...
+     *
+     * @param repository
+     */
+    public CarService(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * ...
+     *
+     * @param id
+     * @param stand
+     * @param license
+     * @param days
+     */
+    public void enterService(int id, int stand, String license, int days) {
+
+        Car car = new Car(id, stand, license, days);
+        List<Car> cars = repository.getAll();
+        for (Car c : cars) {
+            if (c.getStand() == stand && !c.isLeftService()) {
+                throw new RuntimeException("That stand is already taken!");
+            }
+        }
+        repository.add(car);
+    }
+
+    /**
+     * @param stand
+     * @param report
+     * @param price
+     */
+    public void exitService(int stand, String report, double price) {
+        Car carOnStand = null;
+        List<Car> cars = repository.getAll();
+        for (Car c : cars) {
+            if (c.getStand() == stand && !c.isLeftService()) {
+                carOnStand = c;
+            }
+        }
+
+        if (carOnStand != null) {
+            carOnStand.setReport(report);
+            carOnStand.setPrice(price);
+            carOnStand.setLeftService(true);
+            repository.update(carOnStand);
+        } else {
+            throw new RuntimeException("There is no car on the given stand!");
+        }
+    }
+
+    public List<Car> getAll() {
+        return repository.getAll();
+    }
+
+    public Set<Integer> getAllStands() {
+        Set<Integer> stands = new HashSet<>();
+        List<Car> cars = getAll();
+        for (Car car : cars) {
+            stands.add(car.getStand());
+        }
+        return stands;
+    }
+
+    public List<Double> getAllByStand(int stand) {
+        List<Double> prices = new ArrayList<>();
+        for (Car car : getAll()) {
+            if (car.getStand() == stand) {
+                prices.add(car.getPrice());
+            }
+        }
+        return prices;
+    }
+
+    public Double getAverage(List<Double> prices) {
+        double sum = 0;
+        for (Double price : prices) {
+            sum += price;
+        }
+        return sum / prices.size();
+    }
+
+    public Map<Double, Integer> getReports() {
+        Map<Double, Integer> reports = new TreeMap<>();
+        for (Integer stand : getAllStands()) {
+            List<Double> prices = getAllByStand(stand);
+            Double average = getAverage(prices);
+            reports.put(average, stand);
+        }
+
+        Map<Double,Integer> descReports = new TreeMap<>(new Comparator<Double>()
+        {
+            @Override
+            public int compare(Double price1, Double price2) {
+                return price2.compareTo(price1);
+            }
+        });
+        descReports.putAll(reports);
+        return descReports;
+    }
+
+
+}
---
+++ src/UI/Console.java
@@ -0,0 +1,90 @@
+package UI;
+
+import Domain.Car;
+import Service.CarService;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class Console {
+
+    private CarService service;
+    private Scanner scanner;
+
+    public Console(CarService service) {
+        this.service = service;
+        scanner = new Scanner(System.in);
+    }
+
+    private void showMenu() {
+        System.out.println("1. Intrare in service");
+        System.out.println("2. Iesire din service");
+        System.out.println("3. Raport pret standuri");
+        System.out.println("a. Afisare toate masinile");
+        System.out.println("x. Iesire");
+    }
+
+    public void run() {
+
+        while (true) {
+            showMenu();
+            String option = scanner.nextLine();
+            if (option.equals("1")) {
+                handleServiceEntry();
+            } else if (option.equals("2")) {
+                handleServiceExit();
+            } else if (option.equals("3")) {
+                handleStandsReport();
+            } else if (option.equals("a")) {
+                handleShowAll();
+            } else if (option.equals("x")) {
+                break;
+            }
+        }
+    }
+
+    private void handleServiceExit() {
+        try {
+            System.out.println("Dati standul:");
+            int stand = Integer.parseInt(scanner.nextLine());
+            System.out.println("Dati raportul de service:");
+            String report = scanner.nextLine();
+            System.out.println("Dati pretul facturat:");
+            double price = Double.parseDouble(scanner.nextLine());
+
+            service.exitService(stand, report, price);
+        } catch (RuntimeException runtimeException) {
+            System.out.println("Avem erori: " + runtimeException.getMessage());
+        }
+    }
+
+    private void handleStandsReport() {
+        Map<Double, Integer> reports = service.getReports();
+        for (Double price : reports.keySet()) {
+            System.out.println("Stand: " + reports.get(price) + " price: " + price);
+        }
+    }
+
+    private void handleShowAll() {
+        for (Car c : service.getAll())
+            System.out.println(c);
+    }
+
+    private void handleServiceEntry() {
+
+        try {
+            System.out.println("Dati id-ul:");
+            int id = Integer.parseInt(scanner.nextLine());
+            System.out.println("Dati standul:");
+            int stand = Integer.parseInt(scanner.nextLine());
+            System.out.println("Dati numarul de inmatriculare:");
+            String license = scanner.nextLine();
+            System.out.println("Dati numarul de zile:");
+            int days = Integer.parseInt(scanner.nextLine());
+
+            service.enterService(id, stand, license, days);
+        } catch (RuntimeException runtimeException) {
+            System.out.println("Avem erori: " + runtimeException.getMessage());
+        }
+    }
+}
